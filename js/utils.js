82a350ce24fb28069d93636d0d600e8b976a23fc
Vicen <vicentemanzanero.alu@iespacomolla.es>
2022-02-05 17:56:04 +0100

    login a medias

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,8 @@
 'use strict';
-
-export const mostrar = ()=> {
+let d = document;
+export const mostrar = () => {
     let imagenes = ["../img/carrusel_historia/1.jpg", "../img/carrusel_historia/2.jpg", "../img/carrusel_historia/3.jpg", "../img/carrusel_historia/4.jpg", "../img/carrusel_historia/5.jpg", "../img/carrusel_historia/6.jpg"];
-    let d = document;
-    let i = 0;   
+    let i = 0;
     setInterval(() => {
         let img = d.createElement("img");
         img.setAttribute("src", imagenes[i]);
@@ -14,4 +13,25 @@ export const mostrar = ()=> {
             i = 0;
         }
     }, 3000);
+}
+
+
+export const openModal = () => {
+    document.getElementById("backdrop").style.display = "block"
+    document.getElementById("exampleModal").style.display = "block"
+    document.getElementById("exampleModal").classList.add("show")
+}
+export const closeModal = () => {
+    document.getElementById("backdrop").style.display = "none"
+    document.getElementById("exampleModal").style.display = "none"
+    document.getElementById("exampleModal").classList.remove("show")
+}
+// Get the modal
+var modal = document.getElementById('exampleModal');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        closeModal()
+    }
 }

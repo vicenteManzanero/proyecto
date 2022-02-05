9fdbe27c97bd33f7069e1052d8d9049253953d02
Vicen <vicentemanzanero.alu@iespacomolla.es>
2022-02-05 11:43:03 +0100

    Haciendo el menú

--- js/index.js
+++ js/index.js
@@ -1,19 +1,8 @@
 'use strict';
+import * as u from "./utils.js";
+//Nivel 1 Eventos de la página
 window.onload = () => {
     let d = document;
-    let imagenes = ["../img/carrusel_historia/1.jpg", "../img/carrusel_historia/2.jpg", "../img/carrusel_historia/3.jpg", "../img/carrusel_historia/4.jpg", "../img/carrusel_historia/5.jpg", "../img/carrusel_historia/6.jpg"];
-    function mostrar() {
-        let i = 0;   
-        setInterval(() => {
-            let img = d.createElement("img");
-            img.setAttribute("src", imagenes[i]);
-            img.setAttribute("class", "carrusel");
-            d.getElementById('carrusel').innerHTML = img.outerHTML;
-            i++;
-            if (i >= imagenes.length) {
-                i = 0;
-            }
-        }, 3000);
-    }
-    mostrar();
+
+    u.mostrar();
 }
---
+++ js/utils.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export const mostrar = ()=> {
+    let imagenes = ["../img/carrusel_historia/1.jpg", "../img/carrusel_historia/2.jpg", "../img/carrusel_historia/3.jpg", "../img/carrusel_historia/4.jpg", "../img/carrusel_historia/5.jpg", "../img/carrusel_historia/6.jpg"];
+    let d = document;
+    let i = 0;   
+    setInterval(() => {
+        let img = d.createElement("img");
+        img.setAttribute("src", imagenes[i]);
+        img.setAttribute("class", "carrusel");
+        d.getElementById('carrusel').innerHTML = img.outerHTML;
+        i++;
+        if (i >= imagenes.length) {
+            i = 0;
+        }
+    }, 3000);
+}

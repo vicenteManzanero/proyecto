dfa86a281592507f71bcec74697becbd50e5533b
Vicen <vicentemanzanero.alu@iespacomolla.es>
2022-02-06 13:10:18 +0100

    A medias la parte de mostrar los artículos amplados y añadir al carrito

--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,10 @@
 'use strict';
 import { app } from "../../js/fireBase.js";
 import * as u from "./utils.js";
-import {query,getFirestore,collection,where,onSnapshot} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { query, getFirestore, collection, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 const db = getFirestore(app);
 const productos = collection(db, "Productos");
-
+let d = document;
 
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -13,20 +13,44 @@ function getParameterByName(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+//<a href="#" id="crearCuenta" data-toggle="modal" data-target="#exampleModal2">Crear Cuenta</a>
+
 let categoriaActual = getParameterByName('cat');
 
 const obtenerArticulos = async () => {
     try {
         u.crearCabecera();
-        const consulta = await query(productos,where('categoria', '==', categoriaActual));
+        const consulta = await query(productos, where('categoria', '==', categoriaActual));
         const documentos = await onSnapshot(consulta, (col) => {
-            col.docs.map((documento,index) => {
-                u.mostrarProducto(documento.data(),index);
+            col.docs.map((documento, index) => {
+                u.mostrarProducto(documento.data(), index);
+                d.getElementById(`boton${index}`).addEventListener('click', (e) => {
+
+                    console.log(e.target.parentNode.parentNode.children[1].innerHTML);//Aquí va la función de añadir al carrito.
+
+                    if (documento.data().venta === 'peso') {
+                        console.log(e.target.parentNode.parentNode.children[4].children[0].childNodes[0].value);
+                    } else {
+                        console.log(e.target.parentNode.parentNode.children[4].children[0].childNodes[1].value);
+                    }
+                    // console.log(e.target.parentNode.parentNode.children[4].children[0].childNodes[0].value);//Para peso
+                    // console.log(e.target.parentNode.parentNode.children[4].children[0].childNodes[1].value);//Para select
+
+                }, false);
             });
         });
     } catch (error) {
 
-       console.log(error.message); 
+        console.log(error.message);
     }
 };
 obtenerArticulos();
+
+
+//funcion para mostrar los articulos ampiados
+d.getElementById('resultado').addEventListener('click', (e) => {
+   let fila = e.target.parentNode;
+   fila.setAttribute('data-toggle','modal');
+   fila.setAttribute('data-target','modalArticulos');
+console.log(e.target.parentNode);
+}, false);
--- js/utils.js
+++ js/utils.js
@@ -14,38 +14,39 @@ export const mostrar = () => {
         }
     }, 3000);
 }
-export const comprobarDatos=(correo, password, telf="")=>{//Función creada para comprobar los datos de iniciar sesión y registrarse;
-    var comprobacionCorreo=(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(correo));//Comprobamos el correo electrónico.
+export const comprobarDatos = (correo, password, telf = "") => {//Función creada para comprobar los datos de iniciar sesión y registrarse;
+    var comprobacionCorreo = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(correo));//Comprobamos el correo electrónico.
     console.log(correo);
     console.log(password);
-    
-    var comprobarPassword=(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&*-])[A-Za-z\d@$!%*?&]{8,}$/.test(password));//Obligamos a una constraseña segura de mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.
-    var comprobacionTelf=(/(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}/).test(telf);
+
+    var comprobarPassword = (/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&*-])[A-Za-z\d@$!%*?&]{8,}$/.test(password));//Obligamos a una constraseña segura de mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.
+    var comprobacionTelf = (/(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}/).test(telf);
     console.log(comprobacionCorreo);
     console.log(comprobarPassword);
     console.log(comprobacionTelf);
-    if(telf=="") return (comprobarPassword && comprobacionCorreo);//Para el inicio de sesión .
-    else  return (comprobarPassword && comprobacionCorreo && comprobacionTelf);//Para registrase por primera vez.
+    if (telf == "") return (comprobarPassword && comprobacionCorreo);//Para el inicio de sesión .
+    else return (comprobarPassword && comprobacionCorreo && comprobacionTelf);//Para registrase por primera vez.
 
 }
-export const nuevoUsuarioJSON=(Correo,Telf)=>{// Para crear el usuario con el formato deseado.
-    return { correo:Correo,
-        telefono:Telf,
-        pedidos:""
+export const nuevoUsuarioJSON = (Correo, Telf) => {// Para crear el usuario con el formato deseado.
+    return {
+        correo: Correo,
+        telefono: Telf,
+        pedidos: ""
 
     }
 }
-export function mensajesUsuario(texto){//Para comunicar al usuario todo lo que va ocurriendo
-    let div=  document.getElementById("comunicacion_usuario");
+export function mensajesUsuario(texto) {//Para comunicar al usuario todo lo que va ocurriendo
+    let div = document.getElementById("comunicacion_usuario");
     div.classList.remove("hidden");
-    div.innerHTML= texto;
+    div.innerHTML = texto;
     setTimeout(() => {
-       div.classList.add("hidden");   
+        div.classList.add("hidden");
     }, 3000);
-   }
+}
 
 
-export const openModal = (id1,id2) => {
+export const openModal = (id1, id2) => {
     document.getElementById(`${id1}`).style.display = "block"
     document.getElementById(`${id2}`).style.display = "block"
     document.getElementById(`${id2}`).classList.add("show")
@@ -64,36 +65,67 @@ window.onclick = function (event) {
     }
 }
 
-
-export const mostrarProducto = (objeto,n) =>{
+export const mostrarProducto = (objeto, n) => {
     let div = d.createElement('div');
-    div.setAttribute('class','row');
-    div.setAttribute('id',`fila${n}`);
-    div.innerHTML =`
+    div.style.cursor = 'pointer';
+    div.setAttribute('class', 'row ');
+    div.setAttribute('id', `fila${n}`);
+    div.innerHTML = `
     <div class= "col d-flex align-items-center justify-content-center">${objeto.imagen}</div>
     <div class= "col d-flex align-items-center justify-content-center">${objeto.nombre}</div>
-    <div class= "col d-flex align-items-center justify-content-center">${objeto.alérgenos[0]}</div>
-    <div class= "col d-flex align-items-center justify-content-center">${objeto.precio}</div>
-    <div class= "col d-flex align-items-center justify-content-center">
-    <input type="text" class="form-control" placeholder="Cantidad" aria-describedby="basic-addon1">
-    </div>
-    <div class= "col d-flex align-items-center justify-content-center">
-    <button type="button" class="btn btn-success">Añadir</button>
-    </div>
+    <div class= "col d-flex align-items-center justify-content-center" id="alergenos${n}"></div>
+    <div class= "col d-flex align-items-center justify-content-center">${objeto.precio} €</div>
+    <div class= "col d-flex align-items-center justify-content-center" id="tipo${n}">
     
+    </div>
+    <div class= "col d-flex align-items-center justify-content-center">
+    <button type="button" class="btn btn-success" id="boton${n}">Añadir</button>
+    </div>
     `;
     d.getElementById('resultado').appendChild(div);
+    let aler = d.createElement('div');
+    aler.setAttribute('class', 'col d-flex align-items-center justify-content-center');
+    for (let j = 0; j < objeto.alérgenos.length; j++) {
+        aler.innerHTML += `${objeto.alérgenos[j]}<br>`;
+    }
+    d.getElementById(`alergenos${n}`).appendChild(aler);
+    if (objeto.venta === 'peso') {
+        let divTipo = d.createElement('div');
+        divTipo.setAttribute('class', 'col d-flex align-items-center justify-content-center');
+        divTipo.innerHTML = `<input type="text" class="form-control" placeholder="Cantidad" aria-describedby="basic-addon1">`;
+        d.getElementById(`tipo${n}`).appendChild(divTipo);
+    } else {
+        let divTipo = d.createElement('div');
+        divTipo.setAttribute('class', 'col d-flex align-items-center justify-content-center');
+        divTipo.innerHTML = `
+        <select class="form-select" aria-label="Default select example">
+            <option selected>SELECCIONE</option>
+            <option value="0.250">250 g</option>
+            <option value="0.500">500 g</option>
+            <option value="1">1 Kg</option>
+            <option value="2">2 Kg</option>
+        </select>
+        `;
+        d.getElementById(`tipo${n}`).appendChild(divTipo);
+    }
 
 }
-export const crearCabecera = ()=>{
+
+
+
+
+
+
+
+export const crearCabecera = () => {
     let div = d.createElement('div');
-    div.setAttribute('class','row');
+    div.setAttribute('class', 'row');
     div.innerHTML = `
-    <div class= "col d-flex justify-content-center cabeceraArticulo sticky">FOTO</div>
+    <div class= "col d-flex justify-content-center cabeceraArticulo">FOTO</div>
     <div class= "col d-flex justify-content-center cabeceraArticulo">NOMBRE</div>
     <div class= "col d-flex justify-content-center cabeceraArticulo">ALÉRGENOS</div>
     <div class= "col d-flex justify-content-center cabeceraArticulo">PRECIO</div>
     <div class= "col d-flex justify-content-center cabeceraArticulo">CANTIDAD</div>
     <div class= "col d-flex justify-content-center cabeceraArticulo">AÑADIR</div>`;
-    d.getElementById('resultado').appendChild(div);
+    d.getElementById('cabeceraArticulos').appendChild(div);
 }

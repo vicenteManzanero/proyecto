1d40941aff52150fdd247603b1db1e5930698132
Rienirr <90462305+Rienirr@users.noreply.github.com>
2022-02-07 13:07:41 +0100

    back-end a medias

--- back-end/js/back-end.js
+++ back-end/js/back-end.js
@@ -4,10 +4,12 @@
 import { app,autentificacion } from "../../js/fireBase.js";
 import {
   getFirestore,
-  collection, getDocs, query,
+  collection, getDocs, query,onSnapshot,
   where, orderBy, addDoc, doc, getDoc, updateDoc, arrayUnion, arrayRemove, deleteDoc
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { ProductoJSON, mensajesUsuario } from "./plantillas.js";
+import { crearCabecera, mostrarProducto } from "../../js/utils.js";
+
 
 window.onload = () => {
   var d = document;
@@ -25,9 +27,54 @@ window.onload = () => {
   /*Botones */
   var logout= d.getElementById("logout");
   var anyadir = d.getElementById("anyadir");
+  var formAnyadir=d.getElementById("anyadirProducto");
   var modificar = d.getElementById("modificar");
+  var formModificarProducto= d.getElementById("modificarProducto");
   var eliminar = d.getElementById("eliminar");
   var crear = d.getElementById("crear");
+var  divBuscar= d.getElementById("buscarproducto");
+var botonBuscar= d.getElementById("buscarP");
+ var eliminarProducto= d.getElementById("eliminarProducto");
+
+  anyadir.addEventListener("click",()=>{//Para que nos muestre la información necesaria y oculte el resto.
+    if(!anyadir.classList.contains("seleccionado")){
+      anyadir.classList.toggle("seleccionado");
+      formAnyadir.classList.remove("hidden");
+      formModificarProducto.classList.add("hidden");
+      modificar.classList.remove("seleccionado");
+      eliminar.classList.remove("seleccionado");
+      divBuscar.classList.add("hidden");
+      eliminarProducto.classList.add("hidden");
+    }
+  },false);
+
+  modificar.addEventListener("click",()=>{//Para que nos muestre la información necesaria y oculte el resto.
+    if(!modificar.classList.contains("seleccionado")){
+      modificar.classList.toggle("seleccionado");
+      eliminar.classList.remove("seleccionado");
+      anyadir.classList.remove("seleccionado");
+      formAnyadir.classList.add("hidden");
+      crear.classList.add("hidden");
+      formModificarProducto.classList.remove("hidden");
+      divBuscar.classList.remove("hidden");
+      eliminarProducto.classList.add("hidden");
+    }
+  },false);
+
+  eliminar.addEventListener("click",()=>{//Para que nos muestre la información necesaria y oculte el resto.
+    if(!eliminar.classList.contains("seleccionado")){
+      formAnyadir.classList.add("hidden");
+      eliminar.classList.toggle("seleccionado");
+      modificar.classList.remove("seleccionado");
+      anyadir.classList.remove("seleccionado");
+      crear.classList.add("hidden");
+      formModificarProducto.classList.add("hidden");
+      divBuscar.classList.remove("hidden");
+      eliminarProducto.classList.remove("hidden");
+    }
+  },false);
+
+
   crear.addEventListener("click", () => {
     var nombre = d.getElementById("nombreProducto").value;
     var precio = d.getElementById("precio").value;
@@ -39,9 +86,77 @@ window.onload = () => {
     var alergenos = [];
     chequeados.forEach((element) => { alergenos.push(element.value); });
     const productoNuevo = ProductoJSON(nombre, precio, categoria, tipoVenta, IVA, foto, alergenos);
-    //console.log(productoNuevo);
-    guardarProducto(productos, productoNuevo);
+    console.log(productoNuevo);
+  //  guardarProducto(productos, productoNuevo);
   }, false);
+  botonBuscar.addEventListener("click",async()=>{
+    let cabeceraArticulos=d.getElementById("cabeceraArticulos");
+    cabeceraArticulos.innerHTML="";
+    let articulos=d.getElementById("resultado");
+    articulos.innerHTML="";
+   
+
+  
+    try{
+  crearCabecera();
+    const consulta = await query(productos, where('nombre', '!=', ""));
+    let nombreBusqueda=d.getElementById("buscar").value;
+    const documentos = await onSnapshot(consulta, (col) => {
+      col.docs.map((documento, index) => {
+        if(documento.data().nombre.toUpperCase().includes(nombreBusqueda.toUpperCase())) {
+          mostrarProducto(documento.data(), index);
+         
+            d.getElementById(`boton${index}`).addEventListener('click', (e) => {
+              if(modificar.classList.contains("seleccionado")){
+              var chequeados = d.querySelectorAll("#modificarProducto input[type='checkbox']");
+              var alergenos= documento.data().alérgenos;
+              for(let i=0 ; i<chequeados.length;i++){
+                chequeados[i].checked=false;
+                for(let k=0; k<alergenos.length;k++){
+                  console.log( chequeados[i].value==alergenos[k]);
+                  if( chequeados[i].value==alergenos[k]){
+                    console.log(` añado ${ chequeados[i].value}`);
+                    chequeados[i].checked=true;
+                  }           
+                }
+              }
+              d.getElementById("nombreProductoM").value=documento.data().nombre;
+          d.getElementById("precioM").value=documento.data().precio;
+          d.getElementById("categoriaM").value=documento.data().categoria;
+          d.getElementById("imagenM").value=documento.data().imagen;//funciona pero ahora hay muchos productos que no tienen nada definido en este campo
+          let radio =d.querySelectorAll("input[name='radioM']");
+              if(radio[0].value===documento.data().venta) {
+                radio[0].setAttribute("checked"," ");
+                radio[1].removeAttribute("checked");
+              }
+              else {
+                radio[1].setAttribute("checked"," ");
+                radio[0].removeAttribute("checked");
+              }
+       d.getElementById("IVAM").value=documento.data().IVA;
+          /*     if (documento.data().venta === 'peso') {
+                  console.log(e.target.parentNode.parentNode.children[4].children[0].childNodes[0].value);
+              } else {
+                  console.log(e.target.parentNode.parentNode.children[4].children[0].childNodes[1].value);
+              } */
+              
+            }
+            else if(eliminar.classList.contains("seleccionado")){//Añadimos al formulario el nombre a eliminar de la base de datos
+              d.getElementById("nombreProductoE").value=documento.data().nombre;
+            }
+          }, false);
+          
+         
+    
+        }
+      });
+  });    
+}catch (error) {
+console.log(error.message);
+}
+
+  },false);
+
   const cerrarSesion = () => {//Para cerrar la sesión indicada.
     autentificacion
       .signOut()

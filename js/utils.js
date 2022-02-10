676bc4593ee9021c35bdeb9e1261d0de41d9a37b
Rienirr <antoniobonmati.alu@iespacomolla.es>
2022-02-10 16:21:31 +0100

    cambiosListos

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,19 @@
 'use strict';
 let d = document;
+import { app } from "../../js/fireBase.js";
+import {
+    getFirestore,
+    addDoc,  collection
+  } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+  const db = getFirestore(app);
+  const pedidosDB = collection(db, "Pedidos");
+  const guardarPedido = async (pedidos, pedidoCreado) => {
+    const pedidoGuardado = await addDoc(pedidos, pedidoCreado);
+    console.log(`Producto guardado con el id ${pedidoGuardado.id}`);
+    mensajesUsuario(`Pedido añadido con éxito `);
+  };
+
 export const mostrarHistoria = () => {
     let imagenes = ["../img/carrusel_historia/1.jpg", "../img/carrusel_historia/2.jpg", "../img/carrusel_historia/3.jpg", "../img/carrusel_historia/4.jpg", "../img/carrusel_historia/5.jpg", "../img/carrusel_historia/6.jpg"];
     let i = 0;
@@ -18,8 +32,8 @@ export const mostrarHistoria = () => {
 }
 export const comprobarDatos = (correo, password, telf = "") => {//Función creada para comprobar los datos de iniciar sesión y registrarse;
     var comprobacionCorreo = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(correo));//Comprobamos el correo electrónico.
-    console.log(correo);
-    console.log(password);
+  //  console.log(correo);
+   // console.log(password);
 
     var comprobarPassword = (/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&*-])[A-Za-z\d@$!%*?&]{8,}$/.test(password));//Obligamos a una constraseña segura de mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.
     var comprobacionTelf = (/(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}/).test(telf);
@@ -177,13 +191,21 @@ export const crearCabeceraCarrito = (donde) => {//Crea la cabecera para mostrar 
     <div class= "col d-flex justify-content-center cabeceraArticulo">PRODUCTO</div>
     <div class= "col d-flex justify-content-center cabeceraArticulo">PRECIO</div>
     <div class= "col d-flex justify-content-center cabeceraArticulo">CANTIDAD</div>
-    <div class= "col d-flex justify-content-center cabeceraArticulo">TOTAL</div>`;
+    <div class= "col d-flex justify-content-center cabeceraArticulo">TOTAL</div>
+    <div class= "col d-flex justify-content-center cabeceraArticulo">ELIMINAR</div>`;
+    
     cabeceraCarrito.appendChild(div);
+}
+export function PedidosJSON(Correo,Pedido,Fecha){//Creamos los objetos que recibimos por formulario.
+    return {correo: Correo,
+            pedido: Pedido,
+            fecha: Fecha
+        }
 }
 export const enviarProductoCarrito = (carrito, usuarioDelPedido)=>{
     d.getElementById('resultadoCarrito').innerHTML="";//Para cada vez que añadimos o borramos un producto del carrito se vuelva a pintar completamente.
    let totalCompra=0;
-  
+  console.log(carrito);
     carrito.map((objeto)=>{
         let div = d.createElement('div');
         div.setAttribute('class', 'row ');
@@ -192,26 +214,48 @@ export const enviarProductoCarrito = (carrito, usuarioDelPedido)=>{
     <div class= "col d-flex align-items-center justify-content-center">${objeto.precio} €</div>
     <div class= "col d-flex align-items-center justify-content-center">${objeto.cantidad} ${objeto.tipo}</div>
     <div class= "col d-flex align-items-center justify-content-center">${Math.round((objeto.total + Number.EPSILON) * 100) / 100} €</div>
-    `;//El math.Round del final es para que no diera a veces resultados  con muchos decimales;
+    <div class= "col d-flex align-items-center justify-content-center"><img class="iconoBorrar" src="../img/borrar.png" width="30" height="30" /> </div>
+    `;//El math.Round del final es para que no diera a veces resultados  con muchos decimales ya que al ser la conversión a float hay veces que salen cosas raras;
     totalCompra+=Math.round((objeto.total + Number.EPSILON) * 100) / 100;
+    
     d.getElementById('resultadoCarrito').appendChild(div);
 });
+
 let divTotal = d.createElement('div');
 divTotal.setAttribute('class', 'row ');
-divTotal.innerHTML = `  <div class= "col d-flex align-items-center justify-content-center">El precio total del pedido es de ${totalCompra}€ .     Fecha del pedido:   <input type="date" id="fechaPedido">       <button type="button" class="btn btn-success" id="confirmarPedido">confirmar el Pedido</button></div>`;
+divTotal.innerHTML = `  <div class= "col d-flex align-items-center justify-content-center">El precio total del pedido es de ${totalCompra}€ .     Introducir del pedido:   <input type="date" id="fechaPedido">       <button type="button" class="btn btn-success" id="confirmarPedido">confirmar el Pedido</button></div>`;
+
+if(carrito.length==0) {//comprobamos que si borramos producto quede alguno en la lista sino ocultamos la parte de confirmar el pedido.
+   divTotal.setAttribute('class', "hidden");
+   d.getElementById('resultadoCarrito').innerHTML="No hay más productos en el carrito";/*Primero cambiamos el texto del carrito.
+    y luego añadimos el confirmar pedido se hace de esta manera para que no de un error de undefined el addEventListener de realizar pedido aunque el funcionamiento no se vería afectado.*/
+   d.getElementById('resultadoCarrito').appendChild(divTotal);
+
+}
 d.getElementById('resultadoCarrito').appendChild(divTotal);
+
 let botonPedido=d.getElementById("confirmarPedido");
-botonPedido.addEventListener("click",(e)=>{
+let iconosBorrar= d.getElementsByClassName("iconoBorrar");
+for(let i=0; i<iconosBorrar.length; i++){
+  
+    iconosBorrar[i].addEventListener("click",(event)=>{
+        console.log(carrito[i]);
+        carrito.splice(i, 1);//para borrar el elemento del carrito que queremos;
+        console.log( ` longitud carrito ${carrito.length}`);
+       
+        enviarProductoCarrito(carrito, usuarioDelPedido);
+
+    });
+
+}
+botonPedido.addEventListener("click",(e)=>{//Para crear el pedido una vez todos los pasos son correctos.
  let fechasinFiltrar=   d.getElementById("fechaPedido").value;
  let hoy = new Date();
- 
  let fechaPedido = new Date(fechasinFiltrar);
-console.log(fechaPedido.getDay()); //Comparar que no sea 0;
-console.log( fechaPedido.toLocaleDateString());
-console.log(hoy.toLocaleDateString());
-if(fechaPedido>hoy){// falta crear JSON que sea pedido con los datos del carrito e introducirlo en la base de datos.
-    
-     mensajesUsuario('Pedido procesado correctamente.');
+if(fechaPedido>hoy && fechaPedido.getDay()!=0 ){// falta crear JSON que sea pedido con los datos del carrito e introducirlo en la base de datos.
+ guardarPedido(pedidosDB, PedidosJSON(usuarioDelPedido,carrito,fechasinFiltrar));
+ carrito=[];//para vaciarlo por si el usuario quiere hacer otro pedido;
+   carritoVacio();
 }else mensajesUsuario('Recuerde los domingos estamos cerrados y necesitamos un día para la preparación del pedido.');
 
 })
@@ -220,5 +264,5 @@ if(fechaPedido>hoy){// falta crear JSON que sea pedido con los datos del carrito
 
 
 export const carritoVacio = () => {
-    d.getElementById('carrito').innerHTML = 'El carrito está vacio';
+    d.getElementById('resultadoCarrito').innerHTML= 'Pedido Realizado con éxito. Muchas gracias por confiar en nosotros';
 }
